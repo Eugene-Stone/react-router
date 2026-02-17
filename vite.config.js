745db7a4da96769163aa8c0b0bc8625c4556fee2
Eugene-Stone <eugenestone.work@gmail.com>
2026-02-17 12:01:06 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-router'
+  base: '/react-router/'
 //   server: {
 //     open: true, // Проект будет сам открываться в браузере при запуске
 //   }
